95151ad73c1c107535134ce8331203d344e9ba9b
Joel Grenon <joelgrenon@gmail.com>
2022-02-17 18:43:23 -0500

    add more loggin

--- src/lib/project.js
+++ src/lib/project.js
@@ -6,7 +6,9 @@ import chalk from "chalk";
 export async function projectLoader({ path = "./skryb.yaml" } = {}) {
   console.log("loading yaml project spec", path);
   try {
-    return Yaml.load(fs.readFileSync(Path.resolve(path), "utf8"));
+    const config = Yaml.load(fs.readFileSync(Path.resolve(path), "utf8"));
+    console.log("loaded project config", config);
+    return config;
   } catch (err) {
     console.log(chalk.yellowBright.bold("using default project settings. to override, create a `skryb.yaml` file in the current folder"));
     return {
